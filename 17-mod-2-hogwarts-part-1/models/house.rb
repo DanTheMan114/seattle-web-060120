af523f4133941c49e9a5f79561016dd12a44acc6
Flatiron School <soundarya.rajendran@flatironschool.com>
2020-06-17 15:04:53 -0700

    deleting models

--- 17-mod-2-hogwarts-part-1/models/house.rb
+++
@@ -1,9 +0,0 @@
-class House < ActiveRecord::Base
-  has_many :sortings
-  has_many :students, through: :sortings
-
-  def self.display_houses
-    # binding.pry
-    puts "All the houses"
-  end
-end
--- 17-mod-2-hogwarts-part-1/models/student.rb
+++
@@ -1,5 +0,0 @@
-class Student < ActiveRecord::Base
-  has_many :sortings
-  has_many :houses, through: :sortings
-  has_many :supplies
-end
