ccb70b8dbd0400b6ee868a2206cb384307f69fe6
Flatiron School <soundarya.rajendran@flatironschool.com>
2020-06-22 11:24:30 -0700

    Adding code from lecture

--- 18-mod-2-apis/db/seeds.rb
+++ 18-mod-2-apis/db/seeds.rb
@@ -6,8 +6,14 @@ pokemon_stats = RestClient::Request.execute(method: :get,
     "X-RapidAPI-Host" => "pokemon-go1.p.rapidapi.com",
     "X-RapidAPI-Key" => "5db3651e50msha9aec845d0458d1p1c2eefjsn7a066cc59d05"
   })
+  response_json = JSON.parse(pokemon_stats.body)
 
-  binding.pry
+
+  response_json.each do |pokemon|
+    Pokemon.find_or_create_by(pokemon)
+  end
+
+  # binding.pry
 
 
   puts "all seeds are done. woot!"
