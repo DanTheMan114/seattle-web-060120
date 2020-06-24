1a1c70e77f02af7c083cf1fd293594b3602d7870
Flatiron School <soundarya.rajendran@flatironschool.com>
2020-06-24 07:49:58 -0700

    Adding rails url lab

---
+++ 21-rails-url-lab/app/controllers/students_controller.rb
@@ -0,0 +1,29 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: :show
+  
+  def index
+    @students = Student.all
+  end
+
+  def show
+    # byebug
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active == true
+      @student.update(active: false)
+    else 
+      @student.update(active: true)
+    end
+    redirect_to student_path(@student.id)
+    # byebug
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+end
