b4f8a65fec47eb7092b6aeb40f1c0d3213c7dc42
hbj <heyhal19@gmail.com>
2020-08-19 14:15:27 -0700

    css

---
+++ 49-grids-and-flexboxes/SWBATS.md
@@ -0,0 +1,37 @@
+# Welcome to Intro to CSS Grids and Flexboxes!
+
+After this session, students should be able to...
+
+-   Define a grid
+-   Move elements around the grid using various different CSS instructions
+-   Define a flexbox
+-   Move stuff around inside of a flexbox
+-   Think critically about how to debug a CSS problem
+-   Reflect upon the nuanced changes to the human experience, from the invention of simple tools to combat the frustration of a hunter or gatherer, to yourself, repeatedly typing `border: 1px solid red;` to combat the frustration of the intangible reality of computer programming. Are our fears the same?
+
+## Agendas
+```
+Grids:
+    - Make a 4 x 4 grid
+    - Observe its default behavior
+    - Make the button really huge
+    - Move some pets around
+    - Make a 4 x 4 grid, but differently
+
+Flexboxes:
+    - Make one of the items a flexbox
+    - Obeserve its default behavior
+    - Look at the general flow of a flexbox
+    - Why do align and justify switch purposes sometimes?
+        - Discuss the anatomy of a flex-box
+    - flex-wrap some stuff
+    - flex-flow some stuff
+```
+
+## Resources
+-   [CSS Tricks Grids](https://css-tricks.com/snippets/css/complete-guide-grid/)
+-   [CSS Tricks Flexboxes](https://css-tricks.com/snippets/css/a-guide-to-flexbox/)
+-   [Grid Garden](https://cssgridgarden.com/)
+-   [Flexbox Froggy](https://flexboxfroggy.com/)
+-   [www.Google.com](https://www.google.com/)
+-   [Video Recording of Lecture](https://youtu.be/PuBukugQXv0)
---
+++ 49-grids-and-flexboxes/timer.js
@@ -0,0 +1,129 @@
+const main = document.getElementById('main');
+let instrs = ["beans", "river", "viri", "wedge", "sig", "rose", "elio", "max", "reilley", "tabitha", "dot", "stella", "chai", "imber", "jackson"];
+let timerIsRunning = false;
+let interval;
+
+function addInstrs() {
+    for (let i = 0; i < instrs.length; i++) {
+        let name  = instrs[i]
+        let div = document.createElement('div')
+        div.className = 'timerbox'
+        div.id = name
+        let lbl = document.createElement('div')
+        lbl.innerText = name
+        let timer = document.createElement('p')
+        timer.innerText = 0
+        timer.id = `${name} timer`
+        timer.className = 'timer'
+
+        let start = document.createElement('button')
+        start.innerText = `Start ${name}`
+        start.id = `${name} start`
+        start.addEventListener("click", () => startTimer(name))
+
+        let end = document.createElement('button')
+        end.innerText = "End"
+        end.id = `${name} end`
+        end.className = 'hidey'
+        end.addEventListener("click", () => endTimer(name))
+
+        div.appendChild(lbl)
+        div.appendChild(timer)
+        div.appendChild(start)
+        div.appendChild(end)
+        main.appendChild(div)
+    }
+    let endMeeting = document.createElement('button')
+    endMeeting.innerText = 'End Meeting!'
+    endMeeting.id = 'end'
+    endMeeting.addEventListener('click', endTheMeeting)
+    main.prepend(endMeeting)
+}
+
+function startTimer(inst) {
+    if (timerIsRunning) {
+        bigAngryRedAlert()
+    }
+    else {
+        // start timer
+        let timerInQuestion = document.getElementById(`${inst} timer`)
+        interval = setInterval(() => {
+            let current = parseInt(timerInQuestion.innerText)
+            timerInQuestion.innerText = current + 1
+        }, 1000);
+
+        // switch which button is showing
+        let start = document.getElementById(`${inst} start`)
+        start.className = 'hidey'
+        let end = document.getElementById(`${inst} end`)
+        end.className = ''
+
+        timerIsRunning = true
+    }
+
+}
+
+function endTimer(inst) {
+    // switch which button is showing
+    let start = document.getElementById(`${inst} start`)
+    start.className = ''
+    let end = document.getElementById(`${inst} end`)
+    end.className = 'hidey'
+
+    timerIsRunning = false
+
+    // stop timer
+    clearInterval(interval)
+}
+
+function bigAngryRedAlert(){
+    let h1 = document.createElement('h1')
+    h1.style = "color: red"
+    h1.innerText = "ONLY ONE TIMER AT A TIME >:-("
+    main.prepend(h1)
+    setTimeout(function(){ main.removeChild(h1)}, 2500);
+}
+
+function addForm(){
+    let form = document.createElement('form')
+    let lbl = document.createElement('label')
+    lbl.innerText = "Please Enter Names of Attendees, Separated by Commas"
+    let input = document.createElement('input')
+    input.placeholder = "leia, roxane, beyonce..."
+    input.name = "names"
+    let sbmt = document.createElement('button')
+    sbmt.innerText = "Create Timers"
+    sbmt.type = 'submit'
+
+    form.addEventListener('submit', (ev)=> createTimers(ev))
+
+    form.appendChild(lbl)
+    form.appendChild(input)
+    form.appendChild(sbmt)
+
+    main.appendChild(form)
+}
+
+function createTimers(ev){
+    ev.preventDefault()
+    instrs = ev.target.names.value.split(",")
+    main.innerText = ''
+    addInstrs()
+}
+
+function endTheMeeting(){
+    let timers = document.getElementsByClassName("timer")
+    let buttons = document.getElementsByTagName('button')
+    for (let i = 0; i < buttons.length; i++) {
+        // hide all buttons
+        buttons[i].className = 'hidey'
+    }
+    for (let i = 0; i < timers.length; i++) {
+        // turn all timers to minutes
+        let count = parseInt(timers[i].innerText)/60
+        timers[i].innerText = `${count} minute(s)`
+    }
+
+}
+
+addInstrs()
